8786413043c08b4ab17ddb2de458871df7fbafd2
jamesbt365 <jamesbt365@gmail.com>
2022-11-12 10:18:38 +0000

    Do stuff?

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,67 @@
+mod commands;
 
+use crate::commands::ping::*;
 
 use std::env;
+use std::sync::Arc;
+use std::collections::HashSet;
 
+use serenity::http::Http;
 use serenity::async_trait;
+use serenity::client::bridge::gateway::ShardManager;
+use serenity::framework::standard::macros::group;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
+use serenity::framework::StandardFramework;
+
+
+#[group]
+#[commands(ping)]
+
+struct Test;
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
 
 #[tokio::main]
 async fn main() {
     let token = env::var("DISCORD_TOKEN").expect("Cannot find envvar DISCORD_TOKEN");
+
+    let (owners, bot_id) = match Http::new(&token).get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match Http::new(&token).get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        },
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+
 
 
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
+  let framework = StandardFramework::new()
+        .configure(|c| c
+                   .prefix("-")
+                   .on_mention(Some(bot_id))
+                   .owners(owners))
+                   .group(&TEST_GROUP);
+        
     let mut client = Client::builder(&token, intents)
+    .framework(framework)
     .event_handler(Handler)
     .await
     .expect("Failed to create client");
@@ -23,6 +69,18 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register ctrl+c handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
 
 struct Handler;
 
