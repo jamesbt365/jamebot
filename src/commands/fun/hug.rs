ec20bab50df6d3e3b36ac7f8b5d598d37dd212b4
TheOnlyPhoenix <54004241+TheOnlyPhoenix@users.noreply.github.com>
2022-12-10 03:11:54 +0100

    Phoenix hug
    Yes

--- src/commands/fun/hug.rs
+++ src/commands/fun/hug.rs
@@ -4,24 +4,52 @@ use serenity::model::prelude::*;
 use serenity::prelude::*;
 use crate::util::parse_id;
 
+
 const HUGS: &[&str] = &[
-    "https://cdn.discordapp.com/attachments/706370092283461695/1048732201040224336/6c4d1a89eb9bcf38bcba1ae379275384.jpg",
-    "https://cdn.discordapp.com/attachments/706370092283461695/1048732201275113513/00fc0198b5669801a4549fb37d95ca5b.jpg",
-    "https://cdn.discordapp.com/attachments/706370092283461695/1048732201702924388/0ce07377d31ab045a0dc35810832e296.jpg",
-    "https://cdn.discordapp.com/attachments/706370092283461695/1048732201975562250/01fbbe021f6de127947b0b3d8cef0f28.jpg",
-    "https://cdn.discordapp.com/attachments/706370092283461695/1048732202327875724/1ca7b7244fc002d31a5c0b3a80a5e6e3.jpg",
-    "https://cdn.discordapp.com/attachments/706370092283461695/1048732202600513646/9ed8e672cdc7c9b87dba7af1ec965a0a.jpg",
-    "https://cdn.discordapp.com/attachments/706370092283461695/1048732202868932678/31d3117eaefdd4bada76573cd1860baf.jpg",
-    "https://cdn.discordapp.com/attachments/872953485296009316/1048735119835402302/1578763433581.png",
-    "https://cdn.discordapp.com/attachments/872953485296009316/1048735120179343381/1630541183104.jpg",
-    "https://cdn.discordapp.com/attachments/872953485296009316/1048735120468738098/1637626165611.jpg",
-    "https://cdn.discordapp.com/attachments/872953485296009316/1048735120716206162/1638486503625.png",
-    "https://cdn.discordapp.com/attachments/872953485296009316/1048735121047564328/1642977586623.jpg",
-    "https://cdn.discordapp.com/attachments/872953485296009316/1048735121286627358/1643838342089.png",
-    "https://cdn.discordapp.com/attachments/872953485296009316/1048735121584427080/1643620971342.jpg",
+    // Link's additions
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569361829666856/6c4d1a89eb9bcf38bcba1ae379275384.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569361456377856/00fc0198b5669801a4549fb37d95ca5b.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569361066315867/0ce07377d31ab045a0dc35810832e296.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569360730759259/01fbbe021f6de127947b0b3d8cef0f28.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569360730759259/01fbbe021f6de127947b0b3d8cef0f28.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569348181397604/9ed8e672cdc7c9b87dba7af1ec965a0a.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569347883597934/31d3117eaefdd4bada76573cd1860baf.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569347581616219/1578763433581.png",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569347258650674/1630541183104.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569346956656820/1637626165611.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569346595954698/1638486503625.png",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569346197491782/1642977586623.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569345853554768/1643838342089.png",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050569345568350218/1643620971342.jpg",
+    // Trash's additions
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050571812355973171/Hugging_002.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050571812016246896/Hugging_003.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050571811659726910/Hugging_004.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050571811210928178/Hugging_005.png",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050571810862813234/Hugging_006.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050571810535653436/Hugging_007.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050571810212683786/Hugging_008.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572272085254195/Hugging_010.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572318516183091/Hugging_012.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572443426758817/Hugging_013.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572442910871582/Hugging_014.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572442369794138/Hugging_015.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572441891635210/Hugging_016.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572441547722833/Hugging_017.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572441149251635/Hugging_018.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572440197144617/Hugging_021.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572439643488286/Hugging_022.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572438792065105/Hugging_024.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572881597304832/Hugging_026.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572881186279545/Hugging_027.jpg",
+    "https://cdn.discordapp.com/attachments/1050568393251623023/1050572880787804170/Hugging_028.jpg",
+    // Phoenix's additions
+    "https://media.discordapp.net/attachments/829787267992256562/1050956518486458419/image0.jpg”
 ];
 
 #[command]
+#[description("hug your friends!! :3")]
+#[bucket(fun)]
 async fn hug(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 
     let target_id = match args.single::<String>().ok().and_then(parse_id) {
